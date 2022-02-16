a6817029f78e7c021b92b29011d039bf18db9982
mumkymikey <michael.day0621@gmail.com>
2022-02-16 15:59:49 -0600

    initialize backend asp.net core app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cs451_commerce_bank_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
