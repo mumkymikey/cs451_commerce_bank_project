2000eb539efd60c4c73e33748ac7df4a41c6d5f0
mumkymikey <michael.day0621@gmail.com>
2022-03-02 00:46:40 -0600

    connect backend to frontend

--- cs451_commerce_bank_project/src/App.vue
+++ cs451_commerce_bank_project/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <LoginPage />
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/LoginPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginPage
   }
 }
 </script>
---
+++ cs451_commerce_bank_project/src/components/LoginPage.vue
@@ -0,0 +1,50 @@
+﻿<template>
+  <div>
+    <input type="text" v-model="userName" />
+    <button @click="onClickSave">Save</button>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      items: []
+    }
+  },
+  methods: {
+    async onClickSave() {
+      await fetch('https://localhost:3000/user', {
+        method: 'GET',
+        data: this.userName
+      })
+
+      this.userName = ''
+      await this.loadItems();
+    },
+
+    async loadItems() {
+      await fetch('https://localhost:3000/user', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userName: this.userName })
+      })
+
+      this.userName = ''
+    },
+
+    async mounted() {
+      await this.loadItems()
+    }
+  }
+}
+</script>
