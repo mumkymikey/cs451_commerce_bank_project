90fedbacf486fd150d886f4d59dae70d62cc1ca2
mumkymikey <michael.day0621@gmail.com>
2022-03-10 14:53:06 -0600

    add login logic

--- cs451_commerce_bank_project/src/components/LoginPage.vue
+++ cs451_commerce_bank_project/src/components/LoginPage.vue
@@ -53,8 +53,8 @@ export default {
   },
 
   methods: {
-    onLogin() {
-      const response = fetch('https://localhost:3000/login', {
+    async onLogin() {
+      const response = await fetch('https://localhost:3000/login', {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
@@ -62,6 +62,10 @@ export default {
         },
         body: JSON.stringify(this.data)
       })
+
+      if (response.status == 200) {
+        this.$router.push('/notification-rules');
+      }
     },
 
     async loadItems() {
