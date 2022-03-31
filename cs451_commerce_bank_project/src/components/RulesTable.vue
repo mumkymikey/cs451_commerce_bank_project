4a29104ed0b77c3df6868a9990ccc6c067312b50
Odai Athamneh <heyodai@gmail.com>
2022-03-31 12:43:28 -0500

    add RulesTable vue component

---
+++ cs451_commerce_bank_project/src/components/RulesTable.vue
@@ -0,0 +1,65 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+/* eslint-disable */
+import store from "../store.js";
+
+export default {
+  data() {
+    return {
+      data: {
+        userId: "",
+      },
+    };
+  },
+
+  mounted() {
+    this.data.userId = store.userId ?? "";
+    fetch("https://localhost:3000/rules", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.data),
+    })
+      .then(async (response) => {
+        const data = await response.json();
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
