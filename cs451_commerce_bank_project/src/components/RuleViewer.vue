92d9d7ea9d037003612b56ba9043b254a6d68278
Odai Athamneh <heyodai@gmail.com>
2022-04-15 15:38:27 -0500

    add back button to rule viewer page

--- cs451_commerce_bank_project/src/components/RuleViewer.vue
+++ cs451_commerce_bank_project/src/components/RuleViewer.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="rule-view-wrapper">
+    <button type="button" class="btn btn-primary btn-pretty">
+      <a href="/notification-rules"> ⬅ Back to Rules </a>
+    </button>
     <h2>Rule Details</h2>
     <ul>
       <li>Name: {{ rule.name }}</li>
@@ -29,11 +32,48 @@ export default {
       },
     };
   },
-  async created() {
-    const response = await fetch(`https://localhost:3000/rules/single/${this.$route.params.id}`)
+  async mounted() {
+    const response = await fetch(
+      `https://localhost:3000/rules/single/${this.$route.params.id}`
+    );
     if (response.status == 200) {
-        this.rule = await response.json()
-    } 
+      this.rule = await response.json();
+    }
   },
 };
 </script>
+
+<style scoped>
+/* TODO: move this to a css file. */
+.btn-pretty a {
+  color: #fff;
+}
+.btn-pretty {
+  appearance: none;
+  backface-visibility: hidden;
+  background-color: #006747;
+  border-radius: 10px;
+  border-style: none;
+  box-shadow: none;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 1em;
+  font-weight: 500;
+  height: 40px;
+  letter-spacing: normal;
+  line-height: 1.5;
+  margin-bottom: 1.5em;
+  outline: none;
+  overflow: hidden;
+  right: 15px;
+  text-align: center;
+  text-decoration: none;
+  transform: translate3d(0, 0, 0);
+  transition: all 0.3s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: top;
+  white-space: nowrap;
+}
+</style>
