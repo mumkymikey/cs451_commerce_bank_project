7aa2de8dc72f323728c41e262f230646b318acb5
Odai Athamneh <heyodai@gmail.com>
2022-04-16 16:47:46 -0500

    add vuetify package

--- cs451_commerce_bank_project/src/main.js
+++ cs451_commerce_bank_project/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import vuetify from '@/plugins/vuetify' 
 
 import LoginPage from './pages/LoginPage.vue'
 import TransactionPage from './pages/TransactionPage.vue'
@@ -74,5 +75,6 @@ const router = new VueRouter({
 
 new Vue({
   router,
+  vuetify,
   render: h =>h(App)
 }).$mount('#app');
