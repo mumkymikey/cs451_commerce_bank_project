c1983efcb77fa8807593bde74fc30307b6bb07e4
mumkymikey <michael.day0621@gmail.com>
2022-04-15 23:48:38 -0500

    add emailAddress to user db migration

--- cs451_commerce_bank_project/Models/User.cs
+++ cs451_commerce_bank_project/Models/User.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace cs451_commerce_bank_project.Models
 {
@@ -11,6 +10,8 @@ namespace cs451_commerce_bank_project.Models
         public int AccountId { get; set; }
 
         public String Name { get; set; }
+
+        public String EmailAddress { get; set; }
 
         public String Username { get; set; }
 
